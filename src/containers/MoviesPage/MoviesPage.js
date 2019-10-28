a2b000605d53e646e1fefd65c4b62accf07e4f60
Emily Lo <hiemilylo@gmail.com>
2019-10-28 18:50:20 -0400

    Formmatting home page

--- src/containers/MoviesPage/MoviesPage.js
+++ src/containers/MoviesPage/MoviesPage.js
@@ -10,11 +10,23 @@ class MoviesPage extends Component {
     movies: MovieList
   };
 
+  hoverHandler = (img) => () => {
+    console.log('hi!' + ' and img: ' + img)
+  }
+
+  notHover = () => {
+    console.log('left')
+  }
+
   render() {
     let { movies } = this.state;
+    // let imgUrl = this.state.nextImg ? this.state.nextImgSrc : this.state.song.imgSrc;
+    // let divStyle = {
+    //     backgroundImage: 'url(' + imgUrl + ')'
+    // }
 
     return (
-      <div className="container">
+      <div className="container" >
         <br />
         <div className="row">
           {movies.map((movie, idx) => {
@@ -25,7 +37,9 @@ class MoviesPage extends Component {
                   className={"photograph p" + idx}
                   to={`${movie.id}`}
                 >
-                  <div className="movieContainer">
+                  <div className="movieContainer"
+                       onMouseEnter={this.hoverHandler(movie.id)}
+                       onMouseLeave={this.notHover}>
                     <h5 className="CharName">{movie.name}</h5>
                     <img src={movie.photo} alt={movie.name} />
                   </div>
